5ffa6be53f9972d46ca414c3c0225ba38f0148a4
Harrison Liddiard <omniaura5@gmail.com>
2024-07-21 21:59:47 -0400

    default org to folded display

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,6 +20,8 @@
 
 (global-set-key "\C-ca" 'org-agenda)
 
+(setq org-startup-folded t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
