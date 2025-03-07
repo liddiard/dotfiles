89e4a4bd2e185722fe48bfe82c25c0445a042852
Harrison Liddiard <git@harrisonliddiard.com>
2025-03-07 11:33:56 -0500

    update emacs font

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -36,7 +36,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Menlo" :foundry "nil" :slant normal :weight regular :height 140 :width normal)))))
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'dracula t)
