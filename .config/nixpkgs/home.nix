ad1a8ce6763c53487a1599748efacace5e3dcbf5
Harrison Liddiard <git@harrisonliddiard.com>
2025-03-08 09:48:17 -0500

    add initial nix home-manager config

---
+++ .config/nixpkgs/home.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should manage
+  home.username = "liddiard";
+  home.homeDirectory = "/Users/liddiard";
+
+  # Allow unfree packages (necessary for some apps)
+  nixpkgs.config.allowUnfree = true;
+
+  # Packages to install
+  home.packages = with pkgs; [
+    # CLI tools
+    git
+    fish
+    tmux
+    emacs
+    # GUI Applications
+    coconutbattery
+    monitorcontrol
+    obs-studio
+    android-file-transfer
+    postman
+    disk-inventory-x
+    vlc
+    audacity
+    blender
+    transmission_4
+    iterm2
+    vscode
+    realvnc-vnc-viewer
+    appcleaner
+    sqlitebrowser
+    webtorrent_desktop
+    zoom-us
+    arc-browser
+    mountain-duck
+    handbrake
+    utm
+    tor-browser
+    inkscape
+    gimp
+    popcorntime
+    darktable
+    signal-desktop
+    docker
+    whatsapp-for-mac
+    qgis
+  ];
+
+  # This tells Home Manager to manage itself
+  programs.home-manager.enable = true;
+
+  # Specify Home Manager version
+  home.stateVersion = "23.05"; # Update with current version
+}
